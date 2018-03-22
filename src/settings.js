07b26189e1c49447913842813f8f2663a668115e
Adrien Poly <adrienpoly@gmail.com>
2018-03-22 11:14:19 +0100

    start date early march

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const dateFormat = "Y-MM-DD HH:mm:ss";
-export const startDateTime = "2018-03-22 01:05:21";
-// export const startDateTime = "2018-01-01";
+// export const startDateTime = "2018-03-22 01:05:21";
+export const startDateTime = "2018-03-01";
